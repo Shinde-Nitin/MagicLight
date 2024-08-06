b42591a2cf7973701064e231dd692747998ebc82
Shinde-Nitin <nitinshinde9834@gmail.com>
2024-08-06 18:23:02 +0530

    done workd

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function onLight(){
+   let img = document.getElementById('light');
+   img.src = "img/Bulb On.png";
+}
+
+function offLight(){
+    let img = document.getElementById('light');
+    img.src = "img/Bulb off.png"
+}
